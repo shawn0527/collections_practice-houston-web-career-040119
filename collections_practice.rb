45c80d07ffdb8676113951add3d4dbf0645f6b4d
Shawn Ren <doggy-ren@hotmail.com>
2019-04-02 19:00:40 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,36 @@
+def sort_array_asc(arr)
+  arr.sort
+end
 
+def sort_array_desc(arr)
+  arr.sort.reverse
+end
+
+def sort_array_char_count(arr)
+  arr.sort{ |x,y| x.length <=> y.length }
+end
+
+def swap_elements(arr)
+  arr[1], arr[2] = arr[2], arr[1]
+  return arr
+end
+
+def swap_elements_from_to(arr, index, destination_index)
+  arr[index], arr[destination_index] = arr[destination_index], arr[index]
+  return arr
+end
+
+def reverse_array(arr)
+  arr.reverse
+end
+
+def kesha_maker(arr)
+  new_arr = []
+  new_string = ""
+  arr.each do |string|
+    string_arr = string.chars
+    string_arr[2] = "$"
+    new_arr << string_arr.join
+  end
+  return new_arr
+end
--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -30,6 +30,13 @@ describe 'collections practice' do
     end
   end
 
+  # Question 4 Advanced
+  describe '#swap_elements_from_to' do
+    it 'swap elements between index and destination_index' do
+      expect(swap_elements_from_to(["Shawn", "Sherry", "Dolby", "Forest", "Chloe"], 2, 4)).to eq(["Shawn", "Sherry", "Chloe", "Forest", "Dolby"])
+    end
+  end
+
   # Question 5
   describe '#reverse_array' do
     it 'reverse the order of an array of integers' do
